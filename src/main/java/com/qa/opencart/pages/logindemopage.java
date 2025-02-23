a733daffca0908375ecefbc54c9e2178d8b122ef
Vijay1894 <161570642+Vijay1894@users.noreply.github.com>
2025-02-23 09:53:08 +0530

    updated as per comment

--- src/main/java/com/qa/opencart/pages/logindemopage.java
+++ src/main/java/com/qa/opencart/pages/logindemopage.java
@@ -3,8 +3,11 @@ package com.qa.opencart.pages;
 public class logindemopage {
 	
 	public static void main(String[] args) {
-		 int i =0;
+		 int i =10;
 		 int j = 20;
+		 
+		 System.out.println(i+j); 
+		 
 	}
 
 }
