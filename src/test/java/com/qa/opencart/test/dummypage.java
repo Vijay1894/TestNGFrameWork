ecf808a11044091dd7e9ef7ff1ec4f309493e5ad
Vijay1894 <161570642+Vijay1894@users.noreply.github.com>
2025-02-26 22:33:27 +0530

    add new update in dummypage

---
+++ src/test/java/com/qa/opencart/test/dummypage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.test;
+
+public class dummypage {
+
+	public static void main(String[] args) {
+
+     int a = 10;
+     int b = 20;
+     
+     System.out.println(a+b);
+
+	}
+
+}
