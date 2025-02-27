53cbdfd889808d008f791a73e10f7c3af08176c6
Vijay1894 <161570642+Vijay1894@users.noreply.github.com>
2025-02-27 22:34:02 +0530

    added my files

---
+++ src/main/java/com/qa/opencart/pages/webpage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class webpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i =10;
+		int b = 30;
+		
+
+	}
+
+}
