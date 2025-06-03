b2767f4bee0556332483b9da501a82eedaf3b606
Vijay1894 <161570642+Vijay1894@users.noreply.github.com>
2025-06-03 08:45:59 +0530

    add tags

--- src/main/java/com/qa/opencart/pages/Actionpage.java
+++ src/main/java/com/qa/opencart/pages/Actionpage.java
@@ -3,9 +3,13 @@ package com.qa.opencart.pages;
 import org.openqa.selenium.WebDriver;
 
 public class Actionpage {
+	public static void main(String[] args) {
+		
+	
 	 int i = 10;
+	 System.out.println(i);
 	 
 	
-	
+	}
 
 }
